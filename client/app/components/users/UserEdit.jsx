6ee602a17728797420863709835839c30e61b71a
undancer <undancer@gmail.com>
2019-05-27 16:41:25 +0800

    修改代码格式化

--- client/app/components/users/UserEdit.jsx
+++ client/app/components/users/UserEdit.jsx
@@ -9,7 +9,7 @@ import { User } from '@/services/user';
 import { Group } from '@/services/group';
 import { currentUser } from '@/services/auth';
 import { absoluteUrl } from '@/services/utils';
-import {FormattedMessage, injectIntl} from 'react-intl';
+import { FormattedMessage, injectIntl } from 'react-intl';
 import { UserProfile } from '../proptypes';
 import DynamicForm from '../dynamic-form/DynamicForm';
 import ChangePasswordDialog from './ChangePasswordDialog';
